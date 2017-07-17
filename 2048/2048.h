2772daa02f36924a0d3017e741ff0851155ec5d3
QUE-WIN\que-win <queziaa31@gmail.com>
2017-07-17 15:08:37 +0800

    no message

--- 2048/2048.h
+++ 2048/2048.h
@@ -9,7 +9,7 @@
 #ifdef _WIN32 
 #include <conio.h>
 #define CLEAY "cls"//win下的清屏 cls
-#define Dgetc(a) a=getch() //win无缓冲输入
+#define Dgetc(a) a=getch(); //win无缓冲输入
 #else 
 #define CLEAY "clear"  //linux下的清屏 clear
 #define Dgetc(a) system("stty raw");a=getchar();system("stty -raw");  //linux无缓冲输入
